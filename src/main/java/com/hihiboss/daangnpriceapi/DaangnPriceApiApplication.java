204753d9a04525e96b23edae00f928fb94cff7d8
hihiboss <junhui820@gmail.com>
2020-06-08 16:53:50 +0900

    init: Initialize Daangn Price API project

---
+++ src/main/java/com/hihiboss/daangnpriceapi/DaangnPriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hihiboss.daangnpriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnPriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnPriceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hihiboss/daangnpriceapi/DaangnPriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hihiboss.daangnpriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnPriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
